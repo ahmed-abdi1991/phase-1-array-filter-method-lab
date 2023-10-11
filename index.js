cc8ffbbac1f462c400c218eccca8efafb334bafd
ahmed-abdi1991 <squarehajji@gmail.com>
2023-10-11 12:26:03 +0300

    assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  // Use the `filter` method to iterate through the drivers array
+  return drivers.filter(driver => {
+    // Check if the driver's name, converted to lowercase, matches the input name
+    return driver.toLowerCase() === name.toLowerCase();
+  });
+}
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver => {
+    // Check if the driver's name starts with the provided letters (case-insensitive)
+    return driver.toLowerCase().startsWith(letters.toLowerCase());
+  });
+}
+const drivers = [
+  { name: "Ali" },
+  { name: "Abdi" },
+  { name: "merry" },
+  { name: "mercy" }
+];
+function matchName(drivers, nameToMatch) {
+  return drivers.filter(driver => driver.name === nameToMatch);
+}
+const matchedDrivers = matchName(drivers, "Ali");
+console.log(matchedDrivers); // [{ name: "Ali" }]
